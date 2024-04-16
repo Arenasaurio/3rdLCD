45fd60e3538a2a350da22050526cab064e8ae0d8
Eduardo Arenas S <arenasaurio03@gmail.com>
2024-04-15 18:08:20 -0600

    se me paro

--- PyProjects/ecuacionesDiferenciales/edo.py
+++ PyProjects/ecuacionesDiferenciales/edo.py
@@ -1,6 +1,8 @@
 #para el puto pancho
+import numpy as np
 def yprima(x,y):
-    return(-3*x)+(2*y)-5
+    #return(-3*x)+(2*y)-5
+    return np.cos(2*x*y)+(x/3)
 
 def euler(x, y, h):
     yp1=y+(h*yprima(x,y))
@@ -12,3 +14,23 @@ def rungekutta2(x, y, h):
     yp1=y+(k2*h)
     return yp1
 
+def rungekutta3(x, y, h):
+    k1=yprima(x,y)
+    k2=yprima(x+(0.5*h),y+(0.5*k1*h))
+    k3=yprima(x+h, y-(k1*h)+(2*k2*h))
+    yp1=y+((k1+(4*k2)+k3)/6*h)
+    return yp1
+
+def k1(x,y,h):
+    return yprima(x,y)
+
+def k2(x,y,h):
+    k1=yprima(x,y)
+    k2=yprima(x+(0.5*h),y+(0.5*k1*h))
+    return k2
+
+def k3(x,y,h):
+    k1=yprima(x,y)
+    k2=yprima(x+(0.5*h),y+(0.5*k1*h))
+    k3=yprima(x+h, y-(k1*h)+(2*k2*h))
+    return k3
--- PyProjects/ecuacionesDiferenciales/main.py
+++ PyProjects/ecuacionesDiferenciales/main.py
@@ -10,9 +10,16 @@ for i in range(0,iteraciones):
     x+=h
     y=ypp1
     print(ypp1)'''
-    ypp1=edo.rungekutta2(x,y,h)
+    ypp1=edo.rungekutta3(x,y,h)
+    k1=edo.k1(x,y,h)
+    k2=edo.k2(x,y,h)
+    k3=edo.k3(x,y,h)
+   
+    print(f'k1=f({x}, {y})={k1}')
+    print(f'k2=f({x}+1/2({h}), {y}+1/2({k1}*{h}))={k2}')
+    print(f'k3=f({x}+{h}, {y}-({k1}*h)+(2*{k2}*{h}))={k3}')
+    print(f'yi+1={y}+1/6({k1}+4({k2})+{k3})={ypp1}\n')
     x+=h
     y=ypp1
-    print(ypp1)
 
     
